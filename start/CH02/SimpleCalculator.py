da172ae024ced2b56dd5db6eab98e7a645fbc0a4
HSS24 <hetavshah12@gmail.com>
2023-09-25 17:35:59 -0400

    calc

--- start/CH02/SimpleCalculator.py
+++ start/CH02/SimpleCalculator.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python3
 # A simple calculator to show math and conditionals
-# Created 
+# Created by hss24
+
+f_n = int(input("what is the first number?"))
+activity = input("what activity? (+ - * /)")
+s_n = int(input("what is the second number? "))    
+
+if activity == "+":
+    print(f_n + s_n)
+
+if activity == "-":
+    print(f_n - s_n)
+
+if activity == "*":
+    print(f_n * s_n)
+
+if activity == "/":
+    print(f_n / s_n)

8e0fd2bad669539234c6861ffec1acb6d4714697
HSS24 <hetavshah12@gmail.com>
2023-09-29 22:19:12 -0400

    codes

--- start/CH03/Loopy.py
+++ start/CH03/Loopy.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
 # example workign with Loops
 #By 
+
+#students = ["tom", "sarah", "bill", "kate"]
+#for student in students:
+#    print("Hello {0}".format(student))
+#    print("How are you today?")
+
+#for i in range(10):
+#    print(i)
+#    print("192.168.0.{0}".format(i+1))
+
+i = 1
+while i < 6:
+    print(i)
+    i = i + 1
---
+++ start/CH03/loopy1.py
@@ -0,0 +1,11 @@
+
+answer = input("is the weather good? (y/n)")
+
+if answer == "y":
+    i = 0
+    while i < 11:
+        print("yes it is")
+        i = i + 1
+
+print("done")
+

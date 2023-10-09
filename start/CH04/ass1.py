c7646546fd06db9c2d010dc57117548d00357133
HSS24 <hetavshah12@gmail.com>
2023-10-09 14:32:00 -0400

    changes

--- start/CH04/ass1.py
+++ start/CH04/ass1.py
@@ -1,12 +1,17 @@
 name = input("Enter your name: ")
-age = input("Enter your age: ")
-email = input("Enter your email address: ")
+col = input("what is your fav color? ")
+pet = input("what is your first pets name?: ")
+mnam = input("what is your mothers maiden name?: ")
+sch = input("what is your first schools name?: ")
 
 # Create or open a file for writing
-with open('user_info.txt', 'w') as file:
+with open('hackme.txt', 'w') as file:
     # Write user information to the file
     file.write(f"Name: {name}\n")
-    file.write(f"Age: {age}\n")
-    file.write(f"Email: {email}\n")
+    file.write(f"Color: {col}\n")
+    file.write(f"Pet Name: {pet}\n")
+    file.write(f"mother;s maiden name: {mnam}\n")
+    file.write(f"School name: {sch}\n")
 
-print("User information has been saved to 'user_info.txt'")
+
+print("User information has been saved to 'hackme.txt'")
